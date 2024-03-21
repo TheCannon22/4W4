66ef648cf70b7360fd506db78057209c05a6eb59
Alexander Rankov <alexanderrankov@gmail.com>
2024-03-21 15:25:41 -0400

    S7C2: Ajout des modification faites en classe

--- header.php
+++ header.php
@@ -13,7 +13,7 @@
 
 </head>
 <body>
-    <div id="menu" class="global">
+    <div id="menu" class="global clr-agencement-primaire">
         <?php
         // Permet d'ajouter un logo qui a été choisi à l'aide du customizer
         echo get_custom_logo() 
